88110463129f42a19eeb1848773a3bb104954f8f
Geonmo Ryu <geonmo@kisti.re.kr>
2023-02-07 15:31:23 +0900

    Remove trust CA update. It is not necessary.

--- install_pycurl.sh
+++ install_pycurl.sh
@@ -2,9 +2,3 @@
 pip3 uninstall pycurl
 pip3 install --compile --global-option="--with-nss" pycurl
 
-
-
-sudo wget https://cafiles.cern.ch/cafiles/certificates/CERN%20Grid%20Certification%20Authority.crt -O /etc/pki/ca-trust/source/anchors/cern-grid-ca.crt
-sudo update-ca-trust force-enable
-sudo update-ca-trust extract
-
